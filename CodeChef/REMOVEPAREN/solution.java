55d6ec838725c0101295962783fadfd4b2affc97
Prabhat Kale <152779947+kaleprabhat24@users.noreply.github.com>
2026-03-25 23:25:37 +0530

    ✅ Solve Remove Outermost Parentheses Practice Problem in Strings in java [CodeChef]

---
+++ CodeChef/REMOVEPAREN/solution.java
@@ -0,0 +1,16 @@
+public static String removeOuterParentheses(String s) {
+    StringBuilder result = new StringBuilder();
+    int balance = 0;
+
+    for (char c : s.toCharArray()) {
+        if (c == '(') {
+            if (balance > 0) result.append(c);
+            balance++;
+        } else {
+            balance--;
+            if (balance > 0) result.append(c);
+        }
+    }
+
+    return result.toString();
+}
