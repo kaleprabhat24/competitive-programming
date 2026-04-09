6f58d167a4a5f81d7e986783410d7291e64b52bc
Prabhat Kale <152779947+kaleprabhat24@users.noreply.github.com>
2026-04-09 16:31:12 +0530

    ✅ Solve Fitness Practice Problem in Basic Math in java [CodeChef]

---
+++ CodeChef/FIT/solution.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        
+        while (t-- > 0) {
+            int x = scanner.nextInt();
+            System.out.println(10 * x);
+        }
+        
+        scanner.close();
+    }
+}
