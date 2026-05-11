5aa72c49d9dccf9c50e2c405af8ce9d4dfd917e4
Prabhat Kale <152779947+kaleprabhat24@users.noreply.github.com>
2026-05-11 11:18:57 +0530

    ✅ Solve Lucky Seven Practice Problem in 500 difficulty rating in java [CodeChef]

---
+++ CodeChef/LUCKYSEVEN/solution.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        // Use charAt(6) to get the 7th character
+        System.out.println(s.charAt(6));
+    }
+}
