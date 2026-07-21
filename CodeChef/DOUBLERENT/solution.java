5ab9b031c6cd5e4677b87f49a14bddd03b6c7c24
Prabhat Kale <152779947+kaleprabhat24@users.noreply.github.com>
2026-07-21 20:24:02 +0530

    ✅ Solve Double Rent Practice Problem in 500 difficulty rating in java [CodeChef]

---
+++ CodeChef/DOUBLERENT/solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main (String[] args) throws java.lang.Exception {
+        Scanner scanner = new Scanner(System.in);
+        
+        if (scanner.hasNextInt()) {
+            int x = scanner.nextInt();
+            System.out.println(x * 2);
+        }
+        
+        scanner.close();
+    }
+}
