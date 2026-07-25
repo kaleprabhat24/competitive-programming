6e6d85b83295423b0a7c196227c1a54ed8747184
Prabhat Kale <152779947+kaleprabhat24@users.noreply.github.com>
2026-07-26 01:43:25 +0530

    ✅ Solve How many unattempted problems Practice Problem in 500 difficulty rating in java [CodeChef]

---
+++ CodeChef/PRACLIST/solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        System.out.println(x - y);
+    }
+}
