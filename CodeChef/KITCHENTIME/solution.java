37e0e6ba8fccafbe8e038e84da755c8f92de70dc
Prabhat Kale <152779947+kaleprabhat24@users.noreply.github.com>
2026-07-30 23:59:12 +0530

    ✅ Solve Kitchen Timings Practice Problem in 500 difficulty rating in java [CodeChef]

---
+++ CodeChef/KITCHENTIME/solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main (String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in); // Fixed: Changed System.util.in to System.in
+        
+        if (sc.hasNextInt()) {
+            int t = sc.nextInt();
+            
+            while (t-- > 0) {
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+                
+                System.out.println(y - x);
+            }
+        }
+        sc.close();
+    }
+}
