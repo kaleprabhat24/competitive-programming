c375fc165c18527f9c7b047b63917f1916b03663
Prabhat Kale <152779947+kaleprabhat24@users.noreply.github.com>
2026-08-08 13:49:22 +0530

    ✅ Solve 2000 Practice Problem in 500 difficulty rating in java [CodeChef]

---
+++ CodeChef/RIP2000/solution.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main (String[] args) throws java.lang.Exception {
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNextInt()) {
+            int n = scanner.nextInt();
+            System.out.println(n * 4);
+        }
+    }
+}
