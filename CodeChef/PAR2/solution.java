f9f3aae29e32fd3b1f40c03eefa6c84d59dc6fa3
Prabhat Kale <152779947+kaleprabhat24@users.noreply.github.com>
2026-08-12 09:03:20 +0530

    ✅ Solve Parity Practice Problem in 500 difficulty rating in java [CodeChef]

---
+++ CodeChef/PAR2/solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextInt()) {
+            int t = sc.nextInt();
+            while (t-- > 0) {
+                int n = sc.nextInt();
+                if (n % 2 == 0) {
+                    System.out.println("Yes");
+                } else {
+                    System.out.println("No");
+                }
+            }
+        }
+        sc.close();
+    }
+}
