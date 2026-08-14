595193fa7647bff055fe89f70b58c166ecbd2f50
Prabhat Kale <152779947+kaleprabhat24@users.noreply.github.com>
2026-08-14 19:14:17 +0530

    ✅ Solve Counting Words Practice Problem in 500 difficulty rating in java [CodeChef]

---
+++ CodeChef/CNTWRD/solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main (String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextInt()) {
+            int t = sc.nextInt();
+            while (t-- > 0) {
+                int n = sc.nextInt();
+                int m = sc.nextInt();
+                System.out.println(n * m);
+            }
+        }
+        sc.close();
+    }
+}
