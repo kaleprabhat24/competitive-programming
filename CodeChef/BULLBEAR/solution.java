fd5dd3799763a7387d339409fb19a6145caf3135
Prabhat Kale <152779947+kaleprabhat24@users.noreply.github.com>
2026-08-20 15:11:13 +0530

    ✅ Solve Bull or Bear Practice Problem in 500 difficulty rating in java [CodeChef]

---
+++ CodeChef/BULLBEAR/solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main (String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextInt()) {
+            int t = sc.nextInt();
+            while (t-- > 0) {
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+                
+                if (y > x) {
+                    System.out.println("PROFIT");
+                } else if (y < x) {
+                    System.out.println("LOSS");
+                } else {
+                    System.out.println("NEUTRAL");
+                }
+            }
+        }
+        sc.close();
+    }
+}
