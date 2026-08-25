2c64dbdd660fb9b90706af1429ee286cd058a1ff
Prabhat Kale <152779947+kaleprabhat24@users.noreply.github.com>
2026-08-25 19:01:30 +0530

    ✅ Solve Get Subscription Practice Problem in 500 difficulty rating in java [CodeChef]

---
+++ CodeChef/SUBSCRIBE/solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main (String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextInt()) {
+            int t = sc.nextInt();
+            while (t-- > 0) {
+                int x = sc.nextInt();
+                if (x > 30) {
+                    System.out.println("YES");
+                } else {
+                    System.out.println("NO");
+                }
+            }
+        }
+        sc.close();
+    }
+}
