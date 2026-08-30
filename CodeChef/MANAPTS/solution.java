dc0fec8025da667d40efc6f8f8d2929202cc1d3f
Prabhat Kale <152779947+kaleprabhat24@users.noreply.github.com>
2026-08-30 16:27:16 +0530

    ✅ Solve Mana Points Practice Problem in 500 difficulty rating in java [CodeChef]

---
+++ CodeChef/MANAPTS/solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main (String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextInt()) {
+            int t = sc.nextInt();
+            while (t-- > 0) {
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+                
+                // Integer division gives the maximum attacks
+                int attacks = y / x;
+                
+                System.out.println(attacks);
+            }
+        }
+        sc.close();
+    }
+}
