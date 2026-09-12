097917bdac5055b2bfc94d1d9a53e8f15cf4055c
Prabhat Kale <152779947+kaleprabhat24@users.noreply.github.com>
2026-09-12 20:11:42 +0530

    ✅ Solve Chef and Chapters Practice Problem in 500 difficulty rating in java [CodeChef]

--- CodeChef/SEMCOURSES/solution.java
+++ CodeChef/SEMCOURSES/solution.java
@@ -5,14 +5,17 @@ import java.io.*;
 class Codechef {
     public static void main (String[] args) throws java.lang.Exception {
         Scanner sc = new Scanner(System.in);
+        
         if (sc.hasNextInt()) {
             int t = sc.nextInt();
+            
             while (t-- > 0) {
+                // Read three integers for X, Y, and Z
                 int x = sc.nextInt();
                 int y = sc.nextInt();
                 int z = sc.nextInt();
                 
-                // Calculate and print the total number of chapters
+                // Output the product
                 System.out.println(x * y * z);
             }
         }
